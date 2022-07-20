3cf358e84455d93583bb21044ed080f422ca3920
Cristian Alvarado <chalaca23ff@gmail.com>
2022-07-20 15:33:49 -0500

    se agregaron los servicios

--- src/v1/routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express'); 
-const router = express.Router();
-
-router.route("/").get((req, res) => {
-    res.send(`<h2>Hola desde ${req.baseUrl}</h2>`);
-});
-
-module.exports = router; // se exporta la ruta 
